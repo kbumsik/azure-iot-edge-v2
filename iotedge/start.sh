9b39e32d2e1e3c5d0af11eb12ee5cdd6025c42dd
iamoric <pierratlucas@gmail.com>
2019-10-05 18:26:10 +0200

    configure docker socket /var/lib/iotedge/

--- iotedge/start.sh
+++ iotedge/start.sh
@@ -1,10 +1,21 @@
 #!/bin/bash
 
 # Launch docker daemon in our service
-dockerd & 2>/dev/null 
+dockerd & 2>/dev/null
 
 #wait for docker to start.
 sleep 5s
+
+# Configure docker socket
+sudo touch /var/lib/iotedge/mgmt.sock
+sudo touch /var/lib/iotedge/workload.sock
+
+sudo chown iotedge:iotedge /var/lib/iotedge/mgmt.sock
+sudo chown iotedge:iotedge /var/lib/iotedge/workload.sock
+
+sudo chmod 660 /var/lib/iotedge/mgmt.sock
+sudo chmod 666 /var/lib/iotedge/workload.sock
+
 #Launch iotedge
 /usr/bin/iotedged -c config.yaml
 
